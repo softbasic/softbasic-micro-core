34b673c87dea58c9c3ca466b350d2b54965b641c
xiaobo <bingbo1993@126.com>
2021-03-15 17:41:17 +0800

    mongoDao 增加聚合函数分页查询  page方法

--- src/main/java/com/github/softbasic/micro/dao/MongoDao.java
+++ src/main/java/com/github/softbasic/micro/dao/MongoDao.java
@@ -5,11 +5,15 @@ import com.github.softbasic.micro.model.PageVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationOperation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 
 @Repository
 @ConditionalOnProperty(prefix="spring.data.mongodb",name = "enable", havingValue = "true")
@@ -87,6 +91,73 @@ public class MongoDao{
             query.limit(pageSize).skip((pageNo - 1) * pageSize);
             int startIndex = PageVO.getStartOfPage(pageNo, pageSize);
             List result = this.mongoSecondary.find(query, resultClass, collectionName);
+            return new PageVO(startIndex, total, pageSize, result);
+        }
+    }
+
+    /**
+     * 副节点聚合分页查询,表名取自注解或者类名
+     * @param resultClass 返回值封装类
+     * @param operations 聚合函数查询参数
+     * @param pageNo 第几页
+     * @param pageSize 每页的条数
+     * @param <T> 泛型参数
+     * @return
+     */
+    public <T> PageVO<T> page(Class<T> resultClass,List<AggregationOperation> operations,int pageNo, int pageSize) {
+        String collectionName=this.getCollectionName(resultClass);
+        Aggregation aggregation = Aggregation.newAggregation(operations);
+        int total = (int) this.mongoSecondary.aggregate(aggregation, collectionName, resultClass).getMappedResults().size();
+
+        if (total < 1) {
+            return new PageVO();
+        } else {
+            if (pageNo < 1) {
+                pageNo = 1;
+            }
+
+            if (pageSize < 1) {
+                pageSize = total;
+            }
+            operations.add(Aggregation.skip((pageNo - 1) * pageSize));
+            operations.add(Aggregation.limit(pageSize));
+            aggregation = Aggregation.newAggregation(operations);
+            int startIndex = PageVO.getStartOfPage(pageNo, pageSize);
+            List result = this.mongoSecondary.aggregate(aggregation, collectionName, resultClass).getMappedResults();
+            return new PageVO(startIndex, total, pageSize, result);
+        }
+    }
+
+
+    /**
+     * 副节点聚合分页查询
+     * @param resultClass
+     * @param query
+     * @param pageNo
+     * @param pageSize
+     * @param collectionName
+     * @param <T>
+     * @return
+     */
+    public <T> PageVO<T> page(Class<T> resultClass,List<AggregationOperation> operations, int pageNo, int pageSize, String collectionName) {
+        Aggregation aggregation = Aggregation.newAggregation(operations);
+
+        int total = (int) this.mongoSecondary.aggregate(aggregation, collectionName, resultClass).getMappedResults().size();
+        if (total < 1) {
+            return new PageVO();
+        } else {
+            if (pageNo < 1) {
+                pageNo = 1;
+            }
+
+            if (pageSize < 1) {
+                pageSize = total;
+            }
+            operations.add(Aggregation.skip((pageNo - 1) * pageSize));
+            operations.add(Aggregation.limit(pageSize));
+            aggregation = Aggregation.newAggregation(operations);
+            int startIndex = PageVO.getStartOfPage(pageNo, pageSize);
+            List result = this.mongoSecondary.aggregate(aggregation, collectionName, resultClass).getMappedResults();
             return new PageVO(startIndex, total, pageSize, result);
         }
     }
